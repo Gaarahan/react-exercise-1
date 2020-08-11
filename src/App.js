f0240688cc8028f9eaddca4a36d0b68709673ef8
gaarahan <gaarahan@foxmail.com>
2020-08-11 15:46:31 +0800

    [Gaarhan] complete ui design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 import './App.scss';
+import Header from './components/Header/Header';
+import AboutMe from './components/AboutMe/AboutMe';
+import Education from './components/Education/Education';
+import Separator from './components/Separator/Separator';
 
 class App extends Component {
   render() {
-    return <main className="app">start here</main>;
+    return (
+      <main className="app">
+        <Header />
+        <Separator />
+        <AboutMe />
+        <Education />
+      </main>
+    );
   }
 }
 
